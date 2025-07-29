e07994e5996fe941abdfa2746af53b3f1a7c46d9
Houston Braga <houstonliverboy123@gmail.com>
2025-07-29 18:01:33 -0300

    refactor: change component from button to Link

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+
 interface ButtonType {
-  text: string
+  text: string;
 }
 
 export default function Button({ text }: ButtonType) {
   return (
-    <button className="bg-brand px-5 py-3 text-white font-semibold rounded-[8px] cursor-pointer hover:opacity-70 transition-opacity">
+    <Link
+      href="/"
+      className="bg-brand px-5 py-3 text-white font-semibold rounded-[8px] cursor-pointer hover:opacity-70 transition-opacity"
+    >
       {text}
-    </button>
-  )
-};
+    </Link>
+  );
+}
